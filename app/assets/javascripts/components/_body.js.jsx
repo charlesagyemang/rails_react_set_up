075476c5bd78922eddc320f0093670a80ccfd877
charlesagyemang <micnkru@gmail.com>
2019-08-05 14:30:26 +0000

    Feat: Complete reat rails crud:

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -8,7 +8,9 @@ class Body extends React.Component {
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.addNewFruit      = this.addNewFruit.bind(this);
     this.handleDelete     = this.handleDelete.bind(this);
+    this.handleUpdate     = this.handleUpdate.bind(this);
     this.deleteFruit      = this.deleteFruit.bind(this);
+    this.updateFruit      = this.updateFruit.bind(this)
   }
   componentDidMount(){
     fetch('/api/v1/fruits.json')
@@ -52,6 +54,28 @@ class Body extends React.Component {
         })
     }
 
+    handleUpdate(fruit){
+      fetch(`http://localhost:3000/api/v1/fruits/${fruit.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type' : 'application/json',
+          },
+          body: JSON.stringify({fruit: fruit}),
+        })
+        .then((response) => {
+          console.log(response.body);
+          this.updateFruit(fruit);
+        })
+    }
+
+    updateFruit(fruit){
+        let newFruits = this.state.fruits.filter((f) => f.id !== fruit.id);
+        newFruits.push(fruit)
+        this.setState({
+          fruits: newFruits
+        })
+    }
+
     deleteFruit(id){
       console.log(`Item Was Successfully Deleted!${id}`);
       newFruits = this.state.fruits.filter((fruit) => fruit.id !== id)
@@ -69,8 +93,12 @@ class Body extends React.Component {
     render(){
       return(
         <div>
-        <NewFruit handleFormSubmit={this.handleFormSubmit} />
-          <AllFruits fruits={this.state.fruits} handleDelete={this.handleDelete} />
+          <NewFruit handleFormSubmit={this.handleFormSubmit} />
+            <AllFruits
+              fruits        = {this.state.fruits}
+              handleDelete  = {this.handleDelete}
+              handleUpdate  = {this.handleUpdate}
+            />
         </div>
       )
     }
